577fe1b662be47bf86a9f40a2f51da96d6331330
planedob <130969605+planedob@users.noreply.github.com>
2026-06-23 00:13:23 +0800

    chore: configure electron forge packaging

---
+++ forge.config.cjs
@@ -0,0 +1,33 @@
+const { MakerDeb } = require('@electron-forge/maker-deb')
+const { MakerSquirrel } = require('@electron-forge/maker-squirrel')
+const { MakerZIP } = require('@electron-forge/maker-zip')
+
+module.exports = {
+	packagerConfig: {
+		asar: true,
+		name: 'LocalArt Canvas',
+		executableName: 'localart-canvas',
+		extraResource: ['dist', 'config/comfyui-workflow.example.json'],
+		ignore: [
+			/^\/node_modules($|\/)/,
+			/^\/client($|\/)/,
+			/^\/server($|\/)/,
+			/^\/desktop($|\/)/,
+			/^\/docs($|\/)/,
+			/^\/scripts($|\/)/,
+			/^\/canvas($|\/)/,
+			/^\/out($|\/)/,
+			/^\/dist($|\/)/,
+		],
+	},
+	makers: [
+		new MakerSquirrel({ name: 'localart_canvas' }),
+		new MakerZIP({}, ['darwin', 'linux']),
+		new MakerDeb({
+			options: {
+				maintainer: 'LocalArt Canvas contributors',
+				homepage: 'https://github.com/planedob/localart-canvas',
+			},
+		}),
+	],
+}
